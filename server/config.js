797b39c4d6a46a7045c4a0d3d041c6bab431653d
kevinrogerteng <kevin.roger.teng@gmail.com>
2015-09-26 22:36:10 -0700

    setting up db hosts and credentials with dotenv

--- server/config.js
+++ server/config.js
@@ -2,8 +2,15 @@
 require('dotenv').load();
 
 var config = {
+  
   port: 3000,
-  LOCU_API_KEY: process.env.LOCU_API_KEY
+  locu_api_key: process.env.LOCU_API_KEY || "no_api_key",
+  db_config: {
+    'host': process.env.DB_HOST || 'localhost',
+    'username': process.env.DB_USERNAME || 'root',
+    'password': process.env.DB_PASSWORD || 'password'
+  }
+
 };
 
 module.exports = config;
