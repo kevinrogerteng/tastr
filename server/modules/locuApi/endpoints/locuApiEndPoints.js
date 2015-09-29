6626384d3b2a8f2fc9f8c08d39267082d4dcdffe
kevinrogerteng <kevin.roger.teng@gmail.com>
2015-09-28 23:21:05 -0700

    changed up Q using deferred instead

--- server/modules/locuApi/endpoints/locuApiEndPoints.js
+++ server/modules/locuApi/endpoints/locuApiEndPoints.js
@@ -4,9 +4,11 @@ module.exports = function(app){
   var locuService = require('../locuMiddleware.js');
 
   app.get('/api/locu/search', function(req, res){
-    // locuService.queryRestaurant(req, res).then(res.sucess).catch(res.error).done();
-    locuService.queryRestaurant(req, res).then(function(response){
-      console.log(response.success);
-    }).done();
+    locuService.queryRestaurant(req, res)
+      .then(function(response){
+        console.log(response.status);
+      })
+      .catch(res.error)
+      .done();
   });
 };
--- server/modules/locuApi/locuMiddleware.js
+++ server/modules/locuApi/locuMiddleware.js
@@ -7,39 +7,38 @@ var requestify = require('requestify'),
     logger = require('winston');
 
 
-var queryRestaurant = function(req){
+var queryRestaurant = function(req, res){
   //validate the request and see if you have enough information to do a search
   function validateRequest(){
-    return Q.promise(function(resolve, reject){
-      if (true) {
-        resolve({success: 'successful!'});
-      } else {
-        reject();
-      }
-    });
+    var deferred = Q.defer();
+
+    if (true) {
+      deferred.resolve();
+    } else {
+      deferred.reject();
+    }
+
+    return deferred.promise;
   } 
 
   function queryLocuApi(validatedReq){
-    return Q.promise(function(resolve, reject){
-      var requestParams = {
+
+    var deferred = Q.defer();
+    var requestParams = {
         'api_key' : apiKey,
         'fields' : ['name', 'location', 'contact'],
         'venue_queries' : [{
           'name' : 'Gary Danko'
         }]
       }
-
-      var url = 'https://api.locu.com/v2/venue/search';
-
-      requestify.post(url, requestParams).then(function(response){
-        console.log(response);
-        resolve(reponse);
-      }, function(error){
-        console.log(error);
-        reject(error);
-      })
-
+    var url = 'https://api.locu.com/v2/venue/search';
+    requestify.post(url, requestParams).then(function(response){
+      deferred.resolve(response.getBody());
+    }, function(error){
+      deferred.reject(error);
     });
+
+    return deferred.promise
   }
 
   return validateRequest().then(queryLocuApi);
